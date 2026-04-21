091f5dbb501c21a34989245823cd2bbb70dfa641
Eric Blankenhorn <eric@wolfssl.com>
2026-04-21 08:35:00 -0500

    Update testing with a more flexible framework

---
+++ tests/test_mqtt_client.c
@@ -0,0 +1,559 @@
+/* test_mqtt_client.c
+ *
+ * Copyright (C) 2006-2026 wolfSSL Inc.
+ *
+ * This file is part of wolfMQTT.
+ *
+ * wolfMQTT is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * wolfMQTT is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1335, USA
+ */
+
+#ifdef HAVE_CONFIG_H
+    #include <config.h>
+#endif
+
+#include "wolfmqtt/mqtt_client.h"
+#include "tests/unit_test.h"
+
+void run_mqtt_client_tests(void);
+
+/* ============================================================================
+ * Test Fixtures
+ * ============================================================================ */
+
+#define TEST_TX_BUF_SIZE 256
+#define TEST_RX_BUF_SIZE 256
+#define TEST_CMD_TIMEOUT_MS 1000
+
+static MqttClient test_client;
+static MqttNet test_net;
+static byte test_tx_buf[TEST_TX_BUF_SIZE];
+static byte test_rx_buf[TEST_RX_BUF_SIZE];
+
+/* Mock network callbacks - just return errors since we're not actually
+ * connecting to anything */
+static int mock_net_connect(void *context, const char* host, word16 port,
+    int timeout_ms)
+{
+    (void)context; (void)host; (void)port; (void)timeout_ms;
+    return MQTT_CODE_ERROR_NETWORK;
+}
+
+static int mock_net_read(void *context, byte* buf, int buf_len, int timeout_ms)
+{
+    (void)context; (void)buf; (void)buf_len; (void)timeout_ms;
+    return MQTT_CODE_ERROR_NETWORK;
+}
+
+static int mock_net_write(void *context, const byte* buf, int buf_len,
+    int timeout_ms)
+{
+    (void)context; (void)buf; (void)buf_len; (void)timeout_ms;
+    return MQTT_CODE_ERROR_NETWORK;
+}
+
+static int mock_net_disconnect(void *context)
+{
+    (void)context;
+    return MQTT_CODE_SUCCESS;
+}
+
+static void setup(void)
+{
+    XMEMSET(&test_client, 0, sizeof(test_client));
+    XMEMSET(&test_net, 0, sizeof(test_net));
+    XMEMSET(test_tx_buf, 0, sizeof(test_tx_buf));
+    XMEMSET(test_rx_buf, 0, sizeof(test_rx_buf));
+
+    /* Setup mock network callbacks */
+    test_net.connect = mock_net_connect;
+    test_net.read = mock_net_read;
+    test_net.write = mock_net_write;
+    test_net.disconnect = mock_net_disconnect;
+}
+
+static void teardown(void)
+{
+    MqttClient_DeInit(&test_client);
+}
+
+/* ============================================================================
+ * MqttClient_Init Tests
+ * ============================================================================ */
+
+TEST(init_null_client)
+{
+    int rc;
+
+    rc = MqttClient_Init(NULL, &test_net, NULL,
+                         test_tx_buf, TEST_TX_BUF_SIZE,
+                         test_rx_buf, TEST_RX_BUF_SIZE,
+                         TEST_CMD_TIMEOUT_MS);
+    ASSERT_EQ(MQTT_CODE_ERROR_BAD_ARG, rc);
+}
+
+TEST(init_null_tx_buf)
+{
+    int rc;
+
+    rc = MqttClient_Init(&test_client, &test_net, NULL,
+                         NULL, TEST_TX_BUF_SIZE,
+                         test_rx_buf, TEST_RX_BUF_SIZE,
+                         TEST_CMD_TIMEOUT_MS);
+    ASSERT_EQ(MQTT_CODE_ERROR_BAD_ARG, rc);
+}
+
+TEST(init_zero_tx_buf_len)
+{
+    int rc;
+
+    rc = MqttClient_Init(&test_client, &test_net, NULL,
+                         test_tx_buf, 0,
+                         test_rx_buf, TEST_RX_BUF_SIZE,
+                         TEST_CMD_TIMEOUT_MS);
+    ASSERT_EQ(MQTT_CODE_ERROR_BAD_ARG, rc);
+}
+
+TEST(init_null_rx_buf)
+{
+    int rc;
+
+    rc = MqttClient_Init(&test_client, &test_net, NULL,
+                         test_tx_buf, TEST_TX_BUF_SIZE,
+                         NULL, TEST_RX_BUF_SIZE,
+                         TEST_CMD_TIMEOUT_MS);
+    ASSERT_EQ(MQTT_CODE_ERROR_BAD_ARG, rc);
+}
+
+TEST(init_zero_rx_buf_len)
+{
+    int rc;
+
+    rc = MqttClient_Init(&test_client, &test_net, NULL,
+                         test_tx_buf, TEST_TX_BUF_SIZE,
+                         test_rx_buf, 0,
+                         TEST_CMD_TIMEOUT_MS);
+    ASSERT_EQ(MQTT_CODE_ERROR_BAD_ARG, rc);
+}
+
+TEST(init_success)
+{
+    int rc;
+
+    rc = MqttClient_Init(&test_client, &test_net, NULL,
+                         test_tx_buf, TEST_TX_BUF_SIZE,
+                         test_rx_buf, TEST_RX_BUF_SIZE,
+                         TEST_CMD_TIMEOUT_MS);
+    ASSERT_EQ(MQTT_CODE_SUCCESS, rc);
+
+    /* Verify client structure is set up correctly */
+    ASSERT_EQ(test_tx_buf, test_client.tx_buf);
+    ASSERT_EQ(TEST_TX_BUF_SIZE, test_client.tx_buf_len);
+    ASSERT_EQ(test_rx_buf, test_client.rx_buf);
+    ASSERT_EQ(TEST_RX_BUF_SIZE, test_client.rx_buf_len);
+    ASSERT_EQ(TEST_CMD_TIMEOUT_MS, test_client.cmd_timeout_ms);
+}
+
+TEST(init_negative_tx_buf_len)
+{
+    int rc;
+
+    rc = MqttClient_Init(&test_client, &test_net, NULL,
+                         test_tx_buf, -1,
+                         test_rx_buf, TEST_RX_BUF_SIZE,
+                         TEST_CMD_TIMEOUT_MS);
+    ASSERT_EQ(MQTT_CODE_ERROR_BAD_ARG, rc);
+}
+
+/* ============================================================================
+ * MqttClient_DeInit Tests
+ * ============================================================================ */
+
+TEST(deinit_null_client)
+{
+    /* Should not crash with NULL client */
+    MqttClient_DeInit(NULL);
+    /* If we reach here, test passes */
+    ASSERT_TRUE(1);
+}
+
+TEST(deinit_after_init)
+{
+    int rc;
+
+    rc = MqttClient_Init(&test_client, &test_net, NULL,
+                         test_tx_buf, TEST_TX_BUF_SIZE,
+                         test_rx_buf, TEST_RX_BUF_SIZE,
+                         TEST_CMD_TIMEOUT_MS);
+    ASSERT_EQ(MQTT_CODE_SUCCESS, rc);
+
+    /* DeInit should not crash */
+    MqttClient_DeInit(&test_client);
+    ASSERT_TRUE(1);
+}
+
+/* ============================================================================
+ * MqttClient_Connect Tests
+ * ============================================================================ */
+
+TEST(connect_null_client)
+{
+    int rc;
+    MqttConnect connect;
+
+    XMEMSET(&connect, 0, sizeof(connect));
+
+    rc = MqttClient_Connect(NULL, &connect);
+    ASSERT_EQ(MQTT_CODE_ERROR_BAD_ARG, rc);
+}
+
+TEST(connect_null_connect)
+{
+    int rc;
+
+    rc = MqttClient_Init(&test_client, &test_net, NULL,
+                         test_tx_buf, TEST_TX_BUF_SIZE,
+                         test_rx_buf, TEST_RX_BUF_SIZE,
+                         TEST_CMD_TIMEOUT_MS);
+    ASSERT_EQ(MQTT_CODE_SUCCESS, rc);
+
+    rc = MqttClient_Connect(&test_client, NULL);
+    ASSERT_EQ(MQTT_CODE_ERROR_BAD_ARG, rc);
+}
+
+TEST(connect_both_null)
+{
+    int rc;
+
+    rc = MqttClient_Connect(NULL, NULL);
+    ASSERT_EQ(MQTT_CODE_ERROR_BAD_ARG, rc);
+}
+
+TEST(connect_with_mock_network)
+{
+    int rc;
+    MqttConnect connect;
+
+    rc = MqttClient_Init(&test_client, &test_net, NULL,
+                         test_tx_buf, TEST_TX_BUF_SIZE,
+                         test_rx_buf, TEST_RX_BUF_SIZE,
+                         TEST_CMD_TIMEOUT_MS);
+    ASSERT_EQ(MQTT_CODE_SUCCESS, rc);
+
+    XMEMSET(&connect, 0, sizeof(connect));
+    connect.keep_alive_sec = 60;
+    connect.clean_session = 1;
+    connect.client_id = "test_client";
+
+    /* Connect will fail at the network write stage since mock returns error */
+    rc = MqttClient_Connect(&test_client, &connect);
+    /* Should fail with network error since mock network write returns error */
+    ASSERT_TRUE(rc < 0);
+}
+
+/* ============================================================================
+ * MqttClient_Disconnect Tests
+ * ============================================================================ */
+
+TEST(disconnect_null_client)
+{
+    int rc;
+
+    rc = MqttClient_Disconnect(NULL);
+    ASSERT_EQ(MQTT_CODE_ERROR_BAD_ARG, rc);
+}
+
+/* ============================================================================
+ * MqttClient_GetProtocolVersion Tests
+ * ============================================================================ */
+
+TEST(get_protocol_version_default)
+{
+    int rc;
+    int version;
+
+    rc = MqttClient_Init(&test_client, &test_net, NULL,
+                         test_tx_buf, TEST_TX_BUF_SIZE,
+                         test_rx_buf, TEST_RX_BUF_SIZE,
+                         TEST_CMD_TIMEOUT_MS);
+    ASSERT_EQ(MQTT_CODE_SUCCESS, rc);
+
+    version = MqttClient_GetProtocolVersion(&test_client);
+    /* Default protocol version should be 4 (v3.1.1) or 5 (v5.0) depending
+     * on build options */
+#ifdef WOLFMQTT_V5
+    ASSERT_EQ(MQTT_CONNECT_PROTOCOL_LEVEL_5, version);
+#else
+    ASSERT_EQ(MQTT_CONNECT_PROTOCOL_LEVEL_4, version);
+#endif
+}
+
+TEST(get_protocol_version_string)
+{
+    int rc;
+    const char* version_str;
+
+    rc = MqttClient_Init(&test_client, &test_net, NULL,
+                         test_tx_buf, TEST_TX_BUF_SIZE,
+                         test_rx_buf, TEST_RX_BUF_SIZE,
+                         TEST_CMD_TIMEOUT_MS);
+    ASSERT_EQ(MQTT_CODE_SUCCESS, rc);
+
+    version_str = MqttClient_GetProtocolVersionString(&test_client);
+    ASSERT_NOT_NULL(version_str);
+    /* Should be "v3.1.1" or "v5" depending on build options */
+#ifdef WOLFMQTT_V5
+    ASSERT_STR_EQ("v5", version_str);
+#else
+    ASSERT_STR_EQ("v3.1.1", version_str);
+#endif
+}
+
+/* ============================================================================
+ * MqttClient_Ping Tests
+ * ============================================================================ */
+
+TEST(ping_null_client)
+{
+    int rc;
+
+    rc = MqttClient_Ping(NULL);
+    ASSERT_EQ(MQTT_CODE_ERROR_BAD_ARG, rc);
+}
+
+/* ============================================================================
+ * MqttClient_Subscribe Tests
+ * ============================================================================ */
+
+TEST(subscribe_null_client)
+{
+    int rc;
+    MqttSubscribe subscribe;
+
+    XMEMSET(&subscribe, 0, sizeof(subscribe));
+
+    rc = MqttClient_Subscribe(NULL, &subscribe);
+    ASSERT_EQ(MQTT_CODE_ERROR_BAD_ARG, rc);
+}
+
+TEST(subscribe_null_subscribe)
+{
+    int rc;
+
+    rc = MqttClient_Init(&test_client, &test_net, NULL,
+                         test_tx_buf, TEST_TX_BUF_SIZE,
+                         test_rx_buf, TEST_RX_BUF_SIZE,
+                         TEST_CMD_TIMEOUT_MS);
+    ASSERT_EQ(MQTT_CODE_SUCCESS, rc);
+
+    rc = MqttClient_Subscribe(&test_client, NULL);
+    ASSERT_EQ(MQTT_CODE_ERROR_BAD_ARG, rc);
+}
+
+/* ============================================================================
+ * MqttClient_Unsubscribe Tests
+ * ============================================================================ */
+
+TEST(unsubscribe_null_client)
+{
+    int rc;
+    MqttUnsubscribe unsubscribe;
+
+    XMEMSET(&unsubscribe, 0, sizeof(unsubscribe));
+
+    rc = MqttClient_Unsubscribe(NULL, &unsubscribe);
+    ASSERT_EQ(MQTT_CODE_ERROR_BAD_ARG, rc);
+}
+
+TEST(unsubscribe_null_unsubscribe)
+{
+    int rc;
+
+    rc = MqttClient_Init(&test_client, &test_net, NULL,
+                         test_tx_buf, TEST_TX_BUF_SIZE,
+                         test_rx_buf, TEST_RX_BUF_SIZE,
+                         TEST_CMD_TIMEOUT_MS);
+    ASSERT_EQ(MQTT_CODE_SUCCESS, rc);
+
+    rc = MqttClient_Unsubscribe(&test_client, NULL);
+    ASSERT_EQ(MQTT_CODE_ERROR_BAD_ARG, rc);
+}
+
+/* ============================================================================
+ * MqttClient_Publish Tests
+ * ============================================================================ */
+
+TEST(publish_null_client)
+{
+    int rc;
+    MqttPublish publish;
+
+    XMEMSET(&publish, 0, sizeof(publish));
+
+    rc = MqttClient_Publish(NULL, &publish);
+    ASSERT_EQ(MQTT_CODE_ERROR_BAD_ARG, rc);
+}
+
+TEST(publish_null_publish)
+{
+    int rc;
+
+    rc = MqttClient_Init(&test_client, &test_net, NULL,
+                         test_tx_buf, TEST_TX_BUF_SIZE,
+                         test_rx_buf, TEST_RX_BUF_SIZE,
+                         TEST_CMD_TIMEOUT_MS);
+    ASSERT_EQ(MQTT_CODE_SUCCESS, rc);
+
+    rc = MqttClient_Publish(&test_client, NULL);
+    ASSERT_EQ(MQTT_CODE_ERROR_BAD_ARG, rc);
+}
+
+/* ============================================================================
+ * MqttClient_WaitMessage Tests
+ * ============================================================================ */
+
+TEST(wait_message_null_client)
+{
+    int rc;
+
+    rc = MqttClient_WaitMessage(NULL, 1000);
+    ASSERT_EQ(MQTT_CODE_ERROR_BAD_ARG, rc);
+}
+
+/* ============================================================================
+ * MqttClient_ReturnCodeToString Tests
+ * ============================================================================ */
+
+#ifndef WOLFMQTT_NO_ERROR_STRINGS
+TEST(return_code_to_string_success)
+{
+    const char* str;
+
+    str = MqttClient_ReturnCodeToString(MQTT_CODE_SUCCESS);
+    ASSERT_NOT_NULL(str);
+    /* Should contain "Success" or similar */
+    ASSERT_TRUE(str[0] != '\0');
+}
+
+TEST(return_code_to_string_bad_arg)
+{
+    const char* str;
+
+    str = MqttClient_ReturnCodeToString(MQTT_CODE_ERROR_BAD_ARG);
+    ASSERT_NOT_NULL(str);
+    ASSERT_TRUE(str[0] != '\0');
+}
+
+TEST(return_code_to_string_network)
+{
+    const char* str;
+
+    str = MqttClient_ReturnCodeToString(MQTT_CODE_ERROR_NETWORK);
+    ASSERT_NOT_NULL(str);
+    ASSERT_TRUE(str[0] != '\0');
+}
+#endif /* WOLFMQTT_NO_ERROR_STRINGS */
+
+/* ============================================================================
+ * MqttClient_Flags Tests
+ * ============================================================================ */
+
+TEST(client_flags_set_clear)
+{
+    int rc;
+    word32 flags;
+
+    rc = MqttClient_Init(&test_client, &test_net, NULL,
+                         test_tx_buf, TEST_TX_BUF_SIZE,
+                         test_rx_buf, TEST_RX_BUF_SIZE,
+                         TEST_CMD_TIMEOUT_MS);
+    ASSERT_EQ(MQTT_CODE_SUCCESS, rc);
+
+    /* Initially no flags should be set */
+    flags = MqttClient_Flags(&test_client, 0, 0);
+    ASSERT_EQ(0, (int)(flags & MQTT_CLIENT_FLAG_IS_CONNECTED));
+
+    /* Set connected flag */
+    flags = MqttClient_Flags(&test_client, 0, MQTT_CLIENT_FLAG_IS_CONNECTED);
+    ASSERT_TRUE((flags & MQTT_CLIENT_FLAG_IS_CONNECTED) != 0);
+
+    /* Clear connected flag */
+    flags = MqttClient_Flags(&test_client, MQTT_CLIENT_FLAG_IS_CONNECTED, 0);
+    ASSERT_EQ(0, (int)(flags & MQTT_CLIENT_FLAG_IS_CONNECTED));
+}
+
+/* ============================================================================
+ * Test Suite Runner
+ * ============================================================================ */
+
+void run_mqtt_client_tests(void)
+{
+    TEST_SUITE_BEGIN("mqtt_client", setup, teardown);
+
+    /* MqttClient_Init tests */
+    RUN_TEST(init_null_client);
+    RUN_TEST(init_null_tx_buf);
+    RUN_TEST(init_zero_tx_buf_len);
+    RUN_TEST(init_null_rx_buf);
+    RUN_TEST(init_zero_rx_buf_len);
+    RUN_TEST(init_success);
+    RUN_TEST(init_negative_tx_buf_len);
+
+    /* MqttClient_DeInit tests */
+    RUN_TEST(deinit_null_client);
+    RUN_TEST(deinit_after_init);
+
+    /* MqttClient_Connect tests */
+    RUN_TEST(connect_null_client);
+    RUN_TEST(connect_null_connect);
+    RUN_TEST(connect_both_null);
+    RUN_TEST(connect_with_mock_network);
+
+    /* MqttClient_Disconnect tests */
+    RUN_TEST(disconnect_null_client);
+
+    /* MqttClient_GetProtocolVersion tests */
+    RUN_TEST(get_protocol_version_default);
+    RUN_TEST(get_protocol_version_string);
+
+    /* MqttClient_Ping tests */
+    RUN_TEST(ping_null_client);
+
+    /* MqttClient_Subscribe tests */
+    RUN_TEST(subscribe_null_client);
+    RUN_TEST(subscribe_null_subscribe);
+
+    /* MqttClient_Unsubscribe tests */
+    RUN_TEST(unsubscribe_null_client);
+    RUN_TEST(unsubscribe_null_unsubscribe);
+
+    /* MqttClient_Publish tests */
+    RUN_TEST(publish_null_client);
+    RUN_TEST(publish_null_publish);
+
+    /* MqttClient_WaitMessage tests */
+    RUN_TEST(wait_message_null_client);
+
+#ifndef WOLFMQTT_NO_ERROR_STRINGS
+    /* MqttClient_ReturnCodeToString tests */
+    RUN_TEST(return_code_to_string_success);
+    RUN_TEST(return_code_to_string_bad_arg);
+    RUN_TEST(return_code_to_string_network);
+#endif
+
+    /* MqttClient_Flags tests */
+    RUN_TEST(client_flags_set_clear);
+
+    TEST_SUITE_END();
+}
---
+++ tests/unit_test.h
@@ -0,0 +1,294 @@
+/* unit_test.h
+ *
+ * Copyright (C) 2006-2026 wolfSSL Inc.
+ *
+ * This file is part of wolfMQTT.
+ *
+ * wolfMQTT is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * wolfMQTT is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1335, USA
+ */
+
+#ifndef WOLFMQTT_UNIT_TEST_H
+#define WOLFMQTT_UNIT_TEST_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* ============================================================================
+ * Color Output Support
+ * ============================================================================ */
+
+#ifndef UNIT_TEST_NO_COLOR
+    #if defined(_WIN32) || defined(_WIN64)
+        /* Windows - disable colors by default */
+        #define UT_COLOR_RESET   ""
+        #define UT_COLOR_RED     ""
+        #define UT_COLOR_GREEN   ""
+        #define UT_COLOR_YELLOW  ""
+        #define UT_COLOR_CYAN    ""
+    #else
+        /* ANSI escape codes for Unix-like systems */
+        #define UT_COLOR_RESET   "\033[0m"
+        #define UT_COLOR_RED     "\033[1;31m"
+        #define UT_COLOR_GREEN   "\033[1;32m"
+        #define UT_COLOR_YELLOW  "\033[1;33m"
+        #define UT_COLOR_CYAN    "\033[1;36m"
+    #endif
+#else
+    #define UT_COLOR_RESET   ""
+    #define UT_COLOR_RED     ""
+    #define UT_COLOR_GREEN   ""
+    #define UT_COLOR_YELLOW  ""
+    #define UT_COLOR_CYAN    ""
+#endif
+
+/* ============================================================================
+ * Test Framework State
+ *
+ * Global counters are defined in the main translation unit (where
+ * UNIT_TEST_IMPLEMENTATION is defined) and declared as extern elsewhere.
+ * ============================================================================ */
+
+#ifdef UNIT_TEST_IMPLEMENTATION
+    /* Define storage in the main file */
+    int ut_global_pass_count = 0;
+    int ut_global_fail_count = 0;
+#else
+    /* Declare as extern in other files */
+    extern int ut_global_pass_count;
+    extern int ut_global_fail_count;
+#endif
+
+/* Per-suite and per-test state (file-local, OK to be static)
+ * These may be unused in files that only define main(), so mark them as
+ * potentially unused to avoid warnings with strict compilation flags. */
+#if defined(__GNUC__) || defined(__clang__)
+    #define UT_MAYBE_UNUSED __attribute__((unused))
+#else
+    #define UT_MAYBE_UNUSED
+#endif
+
+static int ut_suite_pass_count UT_MAYBE_UNUSED;
+static int ut_suite_fail_count UT_MAYBE_UNUSED;
+static const char* ut_suite_name UT_MAYBE_UNUSED;
+static void (*ut_suite_setup)(void) UT_MAYBE_UNUSED;
+static void (*ut_suite_teardown)(void) UT_MAYBE_UNUSED;
+static int ut_current_test_failed UT_MAYBE_UNUSED;
+
+/* ============================================================================
+ * Assertion Macros
+ * ============================================================================ */
+
+#define ASSERT_TRUE(cond) \
+    do { \
+        if (!(cond)) { \
+            printf("  " UT_COLOR_RED "FAIL" UT_COLOR_RESET ": %s:%d: " \
+                   "ASSERT_TRUE(" #cond ") failed\n", __FILE__, __LINE__); \
+            ut_current_test_failed = 1; \
+            return; \
+        } \
+    } while (0)
+
+#define ASSERT_FALSE(cond) \
+    do { \
+        if (cond) { \
+            printf("  " UT_COLOR_RED "FAIL" UT_COLOR_RESET ": %s:%d: " \
+                   "ASSERT_FALSE(" #cond ") failed\n", __FILE__, __LINE__); \
+            ut_current_test_failed = 1; \
+            return; \
+        } \
+    } while (0)
+
+#define ASSERT_EQ(expected, actual) \
+    do { \
+        long long _exp = (long long)(expected); \
+        long long _act = (long long)(actual); \
+        if (_exp != _act) { \
+            printf("  " UT_COLOR_RED "FAIL" UT_COLOR_RESET ": %s:%d: " \
+                   "ASSERT_EQ(" #expected ", " #actual ") failed: " \
+                   "expected %lld, got %lld\n", __FILE__, __LINE__, _exp, _act); \
+            ut_current_test_failed = 1; \
+            return; \
+        } \
+    } while (0)
+
+#define ASSERT_NE(a, b) \
+    do { \
+        long long _a = (long long)(a); \
+        long long _b = (long long)(b); \
+        if (_a == _b) { \
+            printf("  " UT_COLOR_RED "FAIL" UT_COLOR_RESET ": %s:%d: " \
+                   "ASSERT_NE(" #a ", " #b ") failed: both are %lld\n", \
+                   __FILE__, __LINE__, _a); \
+            ut_current_test_failed = 1; \
+            return; \
+        } \
+    } while (0)
+
+#define ASSERT_NULL(ptr) \
+    do { \
+        if ((ptr) != NULL) { \
+            printf("  " UT_COLOR_RED "FAIL" UT_COLOR_RESET ": %s:%d: " \
+                   "ASSERT_NULL(" #ptr ") failed: pointer is not NULL\n", \
+                   __FILE__, __LINE__); \
+            ut_current_test_failed = 1; \
+            return; \
+        } \
+    } while (0)
+
+#define ASSERT_NOT_NULL(ptr) \
+    do { \
+        if ((ptr) == NULL) { \
+            printf("  " UT_COLOR_RED "FAIL" UT_COLOR_RESET ": %s:%d: " \
+                   "ASSERT_NOT_NULL(" #ptr ") failed: pointer is NULL\n", \
+                   __FILE__, __LINE__); \
+            ut_current_test_failed = 1; \
+            return; \
+        } \
+    } while (0)
+
+#define ASSERT_STR_EQ(expected, actual) \
+    do { \
+        const char* _exp = (expected); \
+        const char* _act = (actual); \
+        if (_exp == NULL || _act == NULL || strcmp(_exp, _act) != 0) { \
+            printf("  " UT_COLOR_RED "FAIL" UT_COLOR_RESET ": %s:%d: " \
+                   "ASSERT_STR_EQ(" #expected ", " #actual ") failed: " \
+                   "expected \"%s\", got \"%s\"\n", __FILE__, __LINE__, \
+                   _exp ? _exp : "(null)", _act ? _act : "(null)"); \
+            ut_current_test_failed = 1; \
+            return; \
+        } \
+    } while (0)
+
+#define ASSERT_MEM_EQ(expected, actual, size) \
+    do { \
+        const void* _exp = (expected); \
+        const void* _act = (actual); \
+        size_t _size = (size); \
+        if (_exp == NULL || _act == NULL || memcmp(_exp, _act, _size) != 0) { \
+            printf("  " UT_COLOR_RED "FAIL" UT_COLOR_RESET ": %s:%d: " \
+                   "ASSERT_MEM_EQ(" #expected ", " #actual ", %zu) failed\n", \
+                   __FILE__, __LINE__, _size); \
+            ut_current_test_failed = 1; \
+            return; \
+        } \
+    } while (0)
+
+#define FAIL(msg) \
+    do { \
+        printf("  " UT_COLOR_RED "FAIL" UT_COLOR_RESET ": %s:%d: %s\n", \
+               __FILE__, __LINE__, (msg)); \
+        ut_current_test_failed = 1; \
+        return; \
+    } while (0)
+
+/* ============================================================================
+ * Test Definition and Execution Macros
+ * ============================================================================ */
+
+/* Define a test function */
+#define TEST(name) static void test_##name(void)
+
+/* Run a single test */
+#define RUN_TEST(name) \
+    do { \
+        ut_current_test_failed = 0; \
+        if (ut_suite_setup) ut_suite_setup(); \
+        test_##name(); \
+        if (ut_suite_teardown) ut_suite_teardown(); \
+        if (ut_current_test_failed) { \
+            ut_suite_fail_count++; \
+            printf("  " UT_COLOR_RED "[FAIL]" UT_COLOR_RESET " %s\n", #name); \
+        } else { \
+            ut_suite_pass_count++; \
+            printf("  " UT_COLOR_GREEN "[PASS]" UT_COLOR_RESET " %s\n", #name); \
+        } \
+    } while (0)
+
+/* Begin a test suite */
+#define TEST_SUITE_BEGIN(name, setup_fn, teardown_fn) \
+    do { \
+        ut_suite_pass_count = 0; \
+        ut_suite_fail_count = 0; \
+        ut_suite_name = (name); \
+        ut_suite_setup = (setup_fn); \
+        ut_suite_teardown = (teardown_fn); \
+        printf("\n" UT_COLOR_CYAN "=== Test Suite: %s ===" UT_COLOR_RESET "\n", \
+               ut_suite_name); \
+    } while (0)
+
+/* End a test suite and report results */
+#define TEST_SUITE_END() \
+    do { \
+        int total = ut_suite_pass_count + ut_suite_fail_count; \
+        printf(UT_COLOR_CYAN "--- Suite Results: %s ---" UT_COLOR_RESET "\n", \
+               ut_suite_name); \
+        printf("  Passed: " UT_COLOR_GREEN "%d" UT_COLOR_RESET \
+               ", Failed: " UT_COLOR_RED "%d" UT_COLOR_RESET \
+               ", Total: %d\n\n", \
+               ut_suite_pass_count, ut_suite_fail_count, total); \
+        ut_global_pass_count += ut_suite_pass_count; \
+        ut_global_fail_count += ut_suite_fail_count; \
+    } while (0)
+
+/* ============================================================================
+ * Global Test Runner Macros
+ * ============================================================================ */
+
+/* Initialize the global test runner */
+#define TEST_RUNNER_BEGIN() \
+    do { \
+        ut_global_pass_count = 0; \
+        ut_global_fail_count = 0; \
+        printf(UT_COLOR_CYAN \
+               "============================================\n" \
+               "         wolfMQTT Unit Test Runner\n" \
+               "============================================" \
+               UT_COLOR_RESET "\n"); \
+    } while (0)
+
+/* Report global results and return exit code */
+#define TEST_RUNNER_END() \
+    do { \
+        int total = ut_global_pass_count + ut_global_fail_count; \
+        printf(UT_COLOR_CYAN \
+               "============================================\n" \
+               "              Final Results\n" \
+               "============================================" \
+               UT_COLOR_RESET "\n"); \
+        printf("  Total Passed: " UT_COLOR_GREEN "%d" UT_COLOR_RESET "\n", \
+               ut_global_pass_count); \
+        printf("  Total Failed: " UT_COLOR_RED "%d" UT_COLOR_RESET "\n", \
+               ut_global_fail_count); \
+        printf("  Total Tests:  %d\n", total); \
+        if (ut_global_fail_count > 0) { \
+            printf("\n" UT_COLOR_RED "TESTS FAILED" UT_COLOR_RESET "\n"); \
+            return 1; \
+        } else { \
+            printf("\n" UT_COLOR_GREEN "ALL TESTS PASSED" UT_COLOR_RESET "\n"); \
+            return 0; \
+        } \
+    } while (0)
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* WOLFMQTT_UNIT_TEST_H */
